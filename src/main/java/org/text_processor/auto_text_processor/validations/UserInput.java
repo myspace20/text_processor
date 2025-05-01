9667c3ae18f99429017cc3954bbbf6b299f1d055
Roger Satsi <rogersatsi98@gmail.com>
2025-05-01 21:55:49 +0000

    refactor(ui): add logging and error handling for ui handlers

--- src/main/java/org/text_processor/auto_text_processor/validations/UserInput.java
+++ src/main/java/org/text_processor/auto_text_processor/validations/UserInput.java
@@ -1,10 +1,12 @@
 package org.text_processor.auto_text_processor.validations;
 
+import org.text_processor.auto_text_processor.exceptions.InvalidInputException;
+
 public class UserInput {
 
-    public static void validateInputField(String value, String field) throws Exception {
+    public static void validateInputField(String value, String field) throws InvalidInputException {
         if(isEmptyOrNullString(value)){
-            throw new Exception(field + " must not be empty");
+            throw new InvalidInputException(field + " must not be empty");
         }
 
     }
