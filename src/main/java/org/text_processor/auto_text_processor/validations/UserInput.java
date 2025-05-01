560850e75d681a08df733abc0b78a236adc8dcf2
Roger Satsi <rogersatsi98@gmail.com>
2025-05-01 20:56:15 +0000

    refactor:improve error in input validation class

--- src/main/java/org/text_processor/auto_text_processor/validations/UserInput.java
+++ src/main/java/org/text_processor/auto_text_processor/validations/UserInput.java
@@ -1,10 +1,12 @@
 package org.text_processor.auto_text_processor.validations;
 
+import org.text_processor.auto_text_processor.exceptions.InvalidInputException;
+
 public class UserInput {
 
     public static void validateInputField(String value, String field) throws Exception {
         if(isEmptyOrNullString(value)){
-            throw new Exception(field + " must not be empty");
+            throw new InvalidInputException(field + " must not be empty");
         }
 
     }
